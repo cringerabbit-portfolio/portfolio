2c5e475a468f4b0b0d361a7def54f05eda55332f
cringerabbit-portfolio <cringerabbit@gmail.com>
2026-09-01 00:56:51 +0900

    Add files via upload

---
+++ assets/js/common.js
@@ -0,0 +1,42 @@
+document.addEventListener('DOMContentLoaded', function () {
+  /* Device Object Size Responsive */
+  rem();
+  window.addEventListener('resize', rem);
+
+  /* Iphone Check */
+  if (navigator.userAgent.match(/iP(hone|od|ad)/)) {
+    document
+      .getElementsByName('viewport')[0]
+      .setAttribute('content', 'width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no, viewport-fit=cover');
+  } else {
+    document
+      .getElementsByName('viewport')[0]
+      .setAttribute('content', 'width=device-width, initial-scale=1.0, viewport-fit=cover');
+  }
+
+  /* Device Check */
+  if (navigator.userAgent.match(/Android|Mobile|iP(hone|od|ad)|BlackBerry|IEMobile|Kindle|NetFront|Silk-Accelerated|(hpw|web)OS|Fennec|Minimo|Opera M(obi|ini)|Blazer|Dolfin|Dolphin|Skyfire|Zune/,)) {
+    document.querySelector(':root').classList.add('dv_mobile');
+  } else {
+    document.querySelector(':root').classList.add('dv_pc');
+  }
+});
+
+/* Device Object Size Responsive */
+function rem() {
+  let clientWidth = document.documentElement.clientWidth;
+  console.log(clientWidth);
+  let breakPoint_pc = 1900;
+  let size_pc = breakPoint_pc;
+  let breakPoint_mobile = 768;
+  let size_mobile = 375;
+  let rem_pc = clientWidth / size_pc;
+  let rem_mobile = clientWidth / size_mobile;
+  if (clientWidth <= breakPoint_mobile) {
+    document.querySelector(':root').style.setProperty('font-size', (rem_mobile * 10) + 'px');
+    // document.querySelector(':root').style.setProperty('font-size', '10px');
+  } else {
+    document.querySelector(':root').style.setProperty('font-size', (rem_pc * 10) + 'px');
+    // document.querySelector(':root').style.setProperty('font-size', '10px');
+  }
+}
